5e3e89304101b9f1e7830f0152e78c2f9b7d49e1
David <davidchincharashvili@gmail.com>
2018-09-01 18:22:49 +0400

    added getAll method
    1 = keys only
    2 = values onlyl
    default object

--- local.js
+++ local.js
@@ -1,16 +1,18 @@
 function DB () {
-    this.key = [];
-    this.value = [];
+   var items = {};
+   var key; 
+   var value;
 
     this.get = function (key) {
         return localStorage[key];
     }
     this.set = function (object) {
+       this.items = {};
         if (typeof object == 'object') {
                 for(element in object) {
                     localStorage.setItem(element,object[element]);
-                    this.key.push(element);
-                    this.value.push(object[element]);
+                    this.key = element;
+                    Object.defineProperty(this.items , element, {value:object[element]});
                 }
         } else{
             console.error('set argument must be object');
@@ -28,4 +30,32 @@ function DB () {
     this.nth = function (number) {
         return localStorage.key(number);
     }
+
+    this.last = function () {
+        return this.items;
+    }
+
+
+    this.getAll = function (keyValue) {
+       var keys = Object.keys(localStorage);
+       var values = Object.values(localStorage);
+        if(keyValue == 1){
+                return keys;
+        }  else if(keyValue == 2 ) {
+                return values;
+        } else{ 
+            var archive = {},   
+                i = keys.length;
+            while ( i-- ) {
+                archive[ keys[i] ] = localStorage.getItem( keys[i] );
+            }
+            return archive;
+        }
+    }
 }
+
+
+var db = new DB;
+
+var query = db.set({'name':"david", 'lname':"chincharashvili"});
+console.log( query.last() );
